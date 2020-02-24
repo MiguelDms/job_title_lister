0f5e27b4b7c242b77715f5567931b8613803a67c
miguel_pereira <miguelglp90@gmail.com>
2020-02-24 15:42:34 +0000

    nwe

--- config/config.php
+++ config/config.php
@@ -5,12 +5,12 @@
 /* define('DB_HOST', "localhost");
 define('DB_USER', "root");
 define('DB_PASSWORD', '');
-define('DB_NAME', 'job_lister');
-define('BASEURL', $_SERVER['DOCUMENT_ROOT']. ''); */
+define('DB_NAME', 'job_lister'); */
+define('BASEURL', $_SERVER['DOCUMENT_ROOT']. '/Job_title_lister'); 
 
 define('DB_USER', 'bbf7143e7c67ac');
 define('DB_PASSWORD', '90906716');
 define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
-define('DB_NAME', 'heroku_d523362820819f8');
+define('DB_NAME', 'heroku_d523362820819f8'); 
 
 define('SITE_TITLE', 'JoBLister');
