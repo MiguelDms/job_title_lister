3344280b48cab0eb99955a00cb830138a6b47a57
miguel_pereira <miguelglp90@gmail.com>
2020-02-25 09:38:35 +0000

    again

--- config/init.php
+++ config/init.php
@@ -21,11 +21,9 @@ spl_autoload_register('myAutoLoaderPerson');
     function myAutoLoaderPerson($className) {
         $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
-        if (strpos($url, "includes") !== false) {
-           $path = '../classes/';
-        } else {
+       
             $path = BASEURL . '/classes/';
-        }
+        
         $extension = '.class.php';
         $fullPath = $path . $className . $extension;
 
