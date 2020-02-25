1f04c36ba9e60bce7cee349f81477168eade6505
miguel_pereira <miguelglp90@gmail.com>
2020-02-25 12:08:42 +0000

    new

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -48,7 +48,7 @@ include '../head.php';
 
             <?php if($logged): ?>
             <li class="nav-item">
-              <a class="nav-link px-1" href="dashboard.php?messages=1"><img src="imagens/iconfinder_message_172503.svg" class="<?php echo (($message_single->checked == '0')? 'new-message' : '') ?>" height="20px" width="30px" alt="message icon"> <span class="sr-only">(current)</span><?php echo (($checked == true)? '<span class="text-danger font-weight-bolder envelope-span">*</span>' : '') ?></a>
+              <a class="nav-link px-1" href="dashboard.php?messages=1"><img src="Imagens/iconfinder_message_172503.svg" class="<?php echo (($message_single->checked == '0')? 'new-message' : '') ?>" height="20px" width="30px" alt="message icon"> <span class="sr-only">(current)</span><?php echo (($checked == true)? '<span class="text-danger font-weight-bolder envelope-span">*</span>' : '') ?></a>
             </li>
             <?php endif; ?>
 
