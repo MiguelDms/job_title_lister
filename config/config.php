e485ba055b2bcb98d0bb87e5583e0afefd987389
miguel_pereira <miguelglp90@gmail.com>
2020-02-25 11:34:20 +0000

    again

--- config/config.php
+++ config/config.php
@@ -7,8 +7,7 @@ define('DB_USER', "root");
 define('DB_PASSWORD', '');
 define('DB_NAME', 'job_lister'); */
 
-define('BASEURL', realpath(__DIR__ . "/../../"));
-
+define('BASEURL', $_SERVER['DOCUMENT_ROOT']); 
 
 define('DB_USER', 'b3fb87a5144f9e');
 define('DB_PASSWORD', 'f0c8d87c');
